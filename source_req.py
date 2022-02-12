ae06a8b7e2c39891c76d334882473124537cbffb
valebanco <bancignano@gmail.com>
2022-02-12 20:42:12 +0100

    Creato ambiente python

---
+++ source_req.py
@@ -0,0 +1,104 @@
+import os
+import glob
+import csv
+import pandas as pd
+import glob
+import pickle
+import numpy
+from keras.utils import np_utils
+import numpy as np
+from numpy.core.fromnumeric import argmax
+from sklearn.model_selection import train_test_split 
+
+import os
+import random
+import sys
+import argparse
+ 
+import music21
+import pandas as pd
+import glob
+import os
+import music21
+
+import os
+import random
+import sys
+import argparse
+ 
+import music21
+import pandas as pd
+
+import pickle
+import numpy
+import keras
+from music21 import instrument, note, stream, chord
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import GRU
+from keras.layers import BatchNormalization as BatchNorm
+from keras.layers import Activation
+
+from keras.layers import Bidirectional
+
+from keras.layers import concatenate
+from keras.layers import Input
+from tensorflow.python.framework.importer import import_graph_def
+
+import pandas as pd
+
+from keras import Model
+
+import matplotlib.pyplot as plt
+import pickle
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import classification_report,confusion_matrix,ConfusionMatrixDisplay
+from keras.models import Sequential,load_model
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import GRU
+from keras.layers import Activation
+from keras.layers import BatchNormalization as BatchNorm
+from keras.layers import concatenate
+from keras.layers import Input
+from keras.layers import Bidirectional
+
+from keras.callbacks import ModelCheckpoint
+
+from keras.optimizers import adam_v2
+
+from keras import Model
+
+import matplotlib.pyplot as plt
+import pickle
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import classification_report,confusion_matrix,ConfusionMatrixDisplay
+from keras.models import Sequential,load_model
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import GRU
+from keras.layers import Activation
+from keras.layers import BatchNormalization as BatchNorm
+from keras.layers import concatenate
+from keras.layers import Input
+from keras.layers import Bidirectional
+
+from keras.callbacks import ModelCheckpoint
+
+from keras.optimizers import adam_v2
+
+from keras import Model
+
+import matplotlib.pyplot as plt
+import numpy as np
+from functools import reduce
+from sklearn.metrics import classification_report,confusion_matrix,ConfusionMatrixDisplay
+import glob,re
+from keras.models import Sequential,load_model
+import pickle
+import plotly.graph_objects as go
